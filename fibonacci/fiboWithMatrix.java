3c2060fd9aa2799605ac1feaaf4e4700ed5978fe
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-01-05 11:37:30 +0200

    fibo

--- fibonacci/fiboWithMatrix.java
+++ fibonacci/fiboWithMatrix.java
@@ -1,4 +1,61 @@
 package fibonacci;
 
 public class fiboWithMatrix {
+
+    /**
+     * Time : O(N)
+     * @param n an index
+     * @return the value in this index
+     */
+    public static int fibMatrix(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        int[][] fiboMatrix = { {1,1}, {1,0} };
+
+        matrixPower(fiboMatrix,n);
+
+        return fiboMatrix[0][0];
+    }
+
+    /**
+     *
+     * @param matrix notation of linear equations represented by matrix
+     *               for the Fibonacci sequence (Fn = Fn-1 + Fn-2)
+     * @param n an index
+     */
+    private static void matrixPower(int[][] matrix, int n) {
+
+        int[][] ans = { {1,1}, {1,0} };
+
+        for (int i = 1; i < n-1; i++) {
+            matrixMultiply(matrix,ans);
+        }
+
+    }
+
+    /**
+     * this is a private method, so we can surely assume that
+     * the multiplying is defined for this matrices.
+     * @param mat1 the original matrix, { {1,1}, {1,0} } .
+     * @param mat2 the copied matrix, multiplied in each call.
+     */
+    private static void matrixMultiply(int[][] mat1, int[][] mat2) {
+
+        int x =  mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
+        int y =  mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
+        int z =  mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0];
+        int w =  mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1];
+
+        mat1[0][0] = x;
+        mat1[0][1] = y;
+        mat1[1][0] = z;
+        mat1[1][1] = w;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fibMatrix(6));
+    }
 }
--- fibonacci/inductive_recursive.java
+++ fibonacci/inductive_recursive.java
@@ -2,13 +2,38 @@ package fibonacci;
 
 public class inductive_recursive {
 
+    /**
+     * 0, 1, 1, 2, 3, 5, 8, ...
+     * Time : O(2^N)
+     * @param n an index
+     * @return the value at this index in fibonacci series
+     */
     public static int fiboRecursive(int n) {
         if( n == 0 || n == 1) {
             return n;
         }
-        return fiboRecursive(n-1) + fiboRecursive(n-2); // O(2^N)
+        return fiboRecursive(n-1) + fiboRecursive(n-2);
     }
 
+    /**
+     * ... −8, 5, −3, 2, −1, 1, 0
+     * Time : O(2^N)
+     * @param n a negative index
+     * @return the value at this index in fibonacci series
+     */
+    public static int negativeFiboRecursive(int n) {
+        if(n >= 0) { // if its not a negative index then use the positive method.
+            return fiboRecursive(n);
+        }
+        return negativeFiboRecursive(n+2) - negativeFiboRecursive(n+1);
+    }
+
+    /**
+     * 0, 1, 1, 2, 3, 5, 8, ...
+     * Time : O(N)
+     * @param n an index
+     * @return the value at this index in fibonacci series
+     */
     public static int fiboInductive(int n) {
         int prev1 = 1;
         int prev2 = 1;
@@ -27,5 +52,6 @@ public class inductive_recursive {
     public static void main(String[] args) {
         System.out.println(fiboRecursive(9));
         System.out.println(fiboInductive(9));
+        System.out.println(negativeFiboRecursive(-7));
     }
 }
