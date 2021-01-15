c191d0c2e51c7d67e99db5f8beed0c409171ced2
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-01-15 13:44:13 +0200

    pdf

--- Fibonacci/fiboWithMatrix.java
+++ Fibonacci/fiboWithMatrix.java
@@ -2,19 +2,21 @@ package Fibonacci;
 
 public class fiboWithMatrix {
 
+
+//////////////// O(N) \\\\\\\\\\\\\\\\\\\\\\
     /**
      * Time : O(N)
      * @param n an index
      * @return the value in this index
      */
-    public static int fibMatrix(int n) {
+    public static int fibMatrixN(int n) {
         if (n <= 0) {
             return 0;
         }
 
         int[][] fiboMatrix = { {1,1}, {1,0} };
 
-        matrixPower(fiboMatrix,n);
+        matrixPowerN(fiboMatrix,n);
 
         return fiboMatrix[0][0];
     }
@@ -25,12 +27,12 @@ public class fiboWithMatrix {
      *               for the Fibonacci sequence (Fn = Fn-1 + Fn-2)
      * @param n an index
      */
-    private static void matrixPower(int[][] matrix, int n) {
+    private static void matrixPowerN(int[][] matrix, int n) {
 
         int[][] ans = { {1,1}, {1,0} };
 
         for (int i = 1; i < n-1; i++) {
-            matrixMultiply(matrix,ans);
+            matrixMultiplyN(matrix,ans);
         }
 
     }
@@ -41,7 +43,68 @@ public class fiboWithMatrix {
      * @param mat1 the original matrix, { {1,1}, {1,0} } .
      * @param mat2 the copied matrix, multiplied in each call.
      */
-    private static void matrixMultiply(int[][] mat1, int[][] mat2) {
+    private static void matrixMultiplyN(int[][] mat1, int[][] mat2) {
+
+        int x =  mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
+        int y =  mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
+        int z =  mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0];
+        int w =  mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1];
+
+        mat1[0][0] = x;
+        mat1[0][1] = y;
+        mat1[1][0] = z;
+        mat1[1][1] = w;
+    }
+//////////////////////////////////////////////////////////////////////////
+
+//////////////// O(log N) \\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * Time : O(log N)
+     * @param n an index
+     * @return the value in this index
+     */
+    public static int fibMatrixLog(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        int[][] fiboMatrix = { {1,1}, {1,0} };
+
+        matrixPowerLog(fiboMatrix,n-1);
+
+        return fiboMatrix[0][0];
+    }
+
+
+    /**
+     *
+     * @param matrix notation of linear equations represented by matrix
+     *               for the Fibonacci sequence (Fn = Fn-1 + Fn-2)
+     * @param n an index
+     */
+    private static void matrixPowerLog(int[][] matrix, int n) {
+
+        if(n == 0 || n == 1)
+            return;
+
+        int[][] ans = { {1,1}, {1,0} };
+
+        matrixPowerLog(matrix, n/2);
+        matrixMultiplyLog(matrix,matrix);
+
+        if(n%2 != 0) {
+            matrixMultiplyLog(matrix,ans);
+        }
+
+    }
+
+    /**
+     * this is a private method, so we can surely assume that
+     * the multiplying is defined for this matrices.
+     * @param mat1 the original matrix, { {1,1}, {1,0} } .
+     * @param mat2 the copied matrix, multiplied in each call.
+     */
+    private static void matrixMultiplyLog(int[][] mat1, int[][] mat2) {
 
         int x =  mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
         int y =  mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
@@ -54,8 +117,11 @@ public class fiboWithMatrix {
         mat1[1][1] = w;
     }
 
-    public static void main(String[] args) {
+//////////////////////////////////////////////////////////////////////////
 
-        System.out.println(fibMatrix(6));
+
+    public static void main(String[] args) {
+        System.out.println(fibMatrixN(6));
+        System.out.println(fibMatrixLog(6));
     }
 }
