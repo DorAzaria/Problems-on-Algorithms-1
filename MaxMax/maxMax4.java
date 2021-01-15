544a6cff507101f6ed52a468513d144372e401ff
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-01-15 12:07:48 +0200

    pdf

--- MaxMax/maxMax4.java
+++ MaxMax/maxMax4.java
@@ -69,9 +69,9 @@ public class maxMax4 {
     public static void main(String[] args) {
         int[] arr = {84, 31, 3, 1, 567, 4, 2, 93202, 32, 3};
         System.out.println(Arrays.toString(maxMax(arr)));
-        arr = new int[] {1,2,3,4,5,8,5,3,2,1,6,7,8};
-        System.out.println(Arrays.toString(maxMax(arr)));
-        arr = new int[] {-1,1,-2,2,-10,10,-100,100,30,2};
-        System.out.println(Arrays.toString(maxMax(arr)));
+            arr = new int[] {1,2,3,4,5,8,5,3,2,1,6,7,8};
+            System.out.println(Arrays.toString(maxMax(arr)));
+            arr = new int[] {-1,1,-2,2,-10,10,-100,100,30,2};
+            System.out.println(Arrays.toString(maxMax(arr)));
     }
 }
