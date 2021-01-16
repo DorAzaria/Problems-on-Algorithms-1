3cde7223f474d0d96ad11d593eb170a18508e220
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-01-16 21:11:10 +0200

    LIS

---
+++ LongestIncreasingSubsequence/LISviaLCS.java
@@ -0,0 +1,12 @@
+package LongestIncreasingSubsequence;
+
+import java.util.Stack;
+
+public class LISviaLCS {
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 100, 101, 2, 3, 4, 5, 6, 7};
+
+    }
+}
